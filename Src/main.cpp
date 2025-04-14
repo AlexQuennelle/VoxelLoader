fee2c58d68d91ef24b918c670d1736d6177717aa
Alex Quennelle <bluephoenixcg@gmail.com>
2025-04-14 14:11:11 -0500

    Add some fancy things to main

--- Src/main.cpp
+++ Src/main.cpp
@@ -1,19 +1,37 @@
 #include "model.h"
 #include "raylib.h"
 #include "volume.h"
+#include <cstring>
+#include <filesystem>
 #include <iostream>
 #include <string>
 
 int main()
 {
+	std::cout << "Chose a file to load\n\n";
+	{
+		namespace fs = std::filesystem;
+		for (const auto& entry : fs::directory_iterator(RESOURCES_PATH))
+		{
+			std::string name =
+				entry.path().string().erase(0, strlen(RESOURCES_PATH));
+			std::cout << '\t' << name << '\n';
+		}
+	}
 	std::string fileName;
+	std::cout << '\n';
 	std::cout << "Enter file name: ";
 	std::cin >> fileName;
-	//fileName = "dragon.vox";
+
+	// TODO: make this fancier later
+	if(!std::filesystem::exists(fileName))
+	{
+		return -1;
+	}
 
 	InitWindow(800, 800, NAME);
 
-	vxl::Model tea(RESOURCES_PATH + fileName);
+	vxl::Model model(RESOURCES_PATH + fileName);
 
 	Camera cam{{150.0f, 150.0f, 150.0f},
 			   {0.0f, 0.0f, 0.0f},
@@ -30,7 +48,7 @@ int main()
 		ClearBackground({100, 149, 237, 255});
 		BeginMode3D(cam);
 
-		vxl::DrawVolume(&tea);
+		vxl::DrawVolume(&model);
 
 		EndMode3D();
 		EndDrawing();
